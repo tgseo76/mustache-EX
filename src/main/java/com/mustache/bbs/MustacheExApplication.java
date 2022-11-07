6c7c73a513decf90dea8411abc845c33eeb845c6
tjxor <seotaegeon@gmail.com>
2022-11-07 11:13:58 +0900

    first commit

---
+++ src/main/java/com/mustache/bbs/MustacheExApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MustacheExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/bbs/MustacheExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
